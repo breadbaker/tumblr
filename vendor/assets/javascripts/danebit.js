de4f4bfdaf8f0f5b281bb6358bc7f5a171fc6c42
App Academy Student <student@appacademy.io>
2013-12-15 21:25:00 -0500

    text editor

--- vendor/assets/javascripts/danebit.js
+++ vendor/assets/javascripts/danebit.js
@@ -1,6 +1,4 @@
 (function () {
-
-
   var root = this;
 
   var _ = root._;
@@ -12,74 +10,164 @@
   _.extend(Editor.prototype, {
     initialize : function(){
       var that = this;
-
-
-
       $('textoptions ul li').on('mousedown', function(e) {
         e.preventDefault();
         var item = $(e.currentTarget);
+        if (!item.hasClass('available')) {
+          return;
+        }
         that.toggleCommandActive(item);
         var action = item.attr('data-action');
-        if(that.specCommands[action]) {
-          console.log('spec');
-        } else {
-
+        if(!that.specCommands(item, action) ) {
           document.execCommand(action);
-
-          that.contextExec(action);
-         // document.execCommand(action);
         }
       });
 
-      // _.each(['focus'], function(eventType){
-//         $('texttextarea').on( eventType, function(e){
-//           that.toggleCommands();
-//         });
-//       })
 
 
-    },
+      $('texttextarea').on('selectstart', function () {
+          $(document).one('mouseup', function() {
+            var s = this.getSelection();
+            if ( s.toString().length > 0 ) {
+              that.saveSelection();
+              $('.needstring').addClass('available');
+              $('.needstring').removeClass('unavailable');
+            } else {
+              $('.needstring').removeClass('available');
+              $('.needstring').addClass('unavailable');
+            }
+          });
+      });
 
-    liveCommands: [],
-
-    specCommands : {
-      'link': function(){
-
-      },
-      'unlink': function(){
-
-      },
-      'photo': function(){
-
-      },
-      'html': function(){
-
-      }
-    },
-
-    contextExec: function(action) {
-      $('texttextarea').focus();
-    },
-
-    toggleCommands: function(){
-      _.each(this.liveCommands, function(command){
-        // var s = document.getSelection();
-//         var n = document.getElementsByTagName('texttextarea')[0];
-//         s.addRange(document.createRange().selectNode(n));
-        document.execCommand(command);
-        console.log('exec', command);
+      $('specbox button').on('click', function(e) {
+        e.preventDefault();
+        if( $('specbox input.url').val() != '' ) {
+          that.restoreSelection();
+          console.log(document.execCommand("createLink", false, $('specbox input.url').val()));
+          that.toggleSpecbox();
+          //range.parentElement().setAttribute("target", "_blank");
+        }
       });
     },
 
-    toggleCommandActive: function(command){
+    saveSelection: function() {
+      var that = this;
+        if (window.getSelection) {
+            sel = window.getSelection();
+            if (sel.getRangeAt && sel.rangeCount) {
+                that.currentRange = sel.getRangeAt(0);
+            }
+        } else if (document.selection && document.selection.createRange) {
+            that.currentRange = document.selection.createRange();
+        }
+        return null;
+    },
 
-      if (_.include(this.liveCommands, command))  {
-        this.liveCommands = _.without(this.liveCommands, command);
+    restoreSelection: function() {
+      var range = this.currentRange;
+        if (range) {
+            if (window.getSelection) {
+                sel = window.getSelection();
+                sel.removeAllRanges();
+                sel.addRange(range);
+            } else if (document.selection && range.select) {
+                range.select();
+            }
+        }
+    },
+
+    hide: function(el, time) {
+      el.addClass('hidden');
+      setTimeout(
+        function(){
+          el.addClass('hide');
+        }
+        , time);
+    },
+    unhide: function(el, time) {
+      el.removeClass('hide');
+      setTimeout(
+        function(){
+          el.removeClass('hidden');
+        }
+        , time);
+    },
+
+    hideShow: function(el1, el2, time){
+      this.hide(el1,time);
+      var that = this;
+      setTimeout(
+        function(){
+          that.unhide(el2,time);
+        }
+        , time);
+    },
+
+    toggleSpecbox: function() {
+      var that = this;
+
+      if ($('specbox').hasClass('modal_active') ) {
+        $('overlay').addClass('hidden');
+        this.hide($('specbox'), 100);
+        $('specbox').addClass('modal_active');
+        this.toggleCommandActive($('.new-link-form'));
+        $('.needstring').removeClass('available');
+        $('.needstring').addClass('unavailable');
+
       } else {
-        this.liveCommands.push(command);
+        $('overlay').removeClass('hidden');
+        that.unhide($('specbox'), 100);
+        $('specbox').addClass('modal_active');
+        $('overlay').on('click', function() {
+          $('overlay').unbind();
+          that.toggleSpecbox();
+        });
       }
+    },
 
-      console.log(this.liveCommands);
+    specCommands : function(item, action){
+      var that = this;
+      actions = {
+        'link': function(){
+          document.execCommand(action);
+          that.toggleCommandActive(item);
+        },
+        'linkForm': function() {
+          that.toggleSpecbox();
+        },
+        'unlink': function(){
+
+        },
+        'photo': function(){
+
+        },
+        'html': function(item, action){
+          if( $('#htmlEdit').hasClass('hidden') ) {
+            $('li.available').addClass('tempunavailable');
+            item.removeClass('tempunavailable');
+            $('#htmlEdit').html($('texttextarea').html());
+            that.hideShow($('texttextarea'),$("#htmlEdit"));
+          } else {
+            $('li.tempunavailable').removeClass('tempunavailable');
+            $('texttextarea').html($('#htmlEdit').val());
+            that.hideShow($("#htmlEdit"), $('texttextarea'));
+          }
+        }
+      };
+      if( actions[action]) {
+        actions[action](item, action);
+        return true;
+      }  else {
+        return false;
+      }
+    },
+
+    toggleCommandActive: function(li) {
+      if (li.hasClass('activated')) {
+        li.removeClass('activated');
+      } else {
+        li.addClass('activated');
+      }
     },
 
   });
